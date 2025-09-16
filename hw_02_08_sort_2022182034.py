67612cf9bf594e1c0623e57895dbfe79ad55cf47
FakeZero(Algorithm_PC) <sjlimdaa@naver.com>
2025-09-16 14:03:25 +0900

    과제 파일

--- hw_02_08_sort_2022182034.py
+++ hw_02_08_sort_2022182034.py
@@ -9,22 +9,59 @@ array = [
 def sort_bubble(arr):
   print('=' * 60)
   print(f'before: {arr}')
+  length = len(arr)
+  for count in range(length - 1):
+      for i in range(length - count - 1):
+          if arr[i] > arr[i + 1]:
+              arr[i], arr[i + 1] = arr[i + 1], arr[i]
   print(f'after : {arr}')
 
 def sort_select(arr):
   print('=' * 60)
   print(f'before: {arr}')
+  length = len(arr)
+  for count in range(length - 1):
+      min = arr[count]
+      for i in range(count + 1, length):
+          if arr[i] < min:
+              min = arr[i]
+              arr[i], arr[count] = arr[count], arr[i]
   print(f'after : {arr}')
 
 def sort_insert(arr):
   print('=' * 60)
   print(f'before: {arr}')
+  length = len(arr)
+  for count in range(1, length):
+      temp = arr[count]
+      i = count
+      while i > 0:
+          if arr[i - 1] > temp:
+              arr[i] = arr[i - 1]
+          else:
+              break
+          i -= 1
+      arr[i] = temp
   print(f'after : {arr}')
 
 def sort_shell(arr):
-  print('=' * 60)
-  print(f'before: {arr}')
-  print(f'after : {arr}')
+    print('=' * 60)
+    print(f'before: {arr}')
+    GAPS = [23, 10, 4, 1]
+    length = len(arr)
+    for gap in GAPS:
+        for start in range(gap, length):
+            temp = arr[start]
+            i = start
+            while i >= gap:
+                if arr[i - gap] > temp:
+                    arr[i] = arr[i - gap]
+                    i -= gap
+                else:
+                    break
+            arr[i] = temp
+
+    print(f'after : {arr}')
 
 def main():
   sort_bubble(array[:])
