68c184cda4e71082171096c229551c2487f0701d
FakeZero(Algorithm_PC) <sjlimdaa@naver.com>
2025-09-30 14:06:38 +0900

    과제 중간

--- hw_02_14_sort_2022182034.py
+++ hw_02_14_sort_2022182034.py
@@ -1,3 +1,5 @@
+from _pyrepl.commands import end
+
 names = [
 
     "임성진",
@@ -17,24 +19,97 @@ names = [
 ]
 
 def sort_insert(arr, start, end_inclusive):
-  print('=' * 60)
-  print(f'before: {arr}')
-  length = end_inclusive + 1
-  for count in range(start + 1, length):
-      temp = arr[count]
-      i = count
-      while i > 0:
-          if arr[i - 1] > temp:
-              arr[i] = arr[i - 1]
-          else:
-              break
-          i -= 1
-      arr[i] = temp
-  print(f'after : {arr}')
+    length = end_inclusive + 1
+    for count in range(start + 1, length):
+        temp = arr[count]
+        i = count
+        while i > 0:
+            if arr[i - 1] > temp:
+                arr[i] = arr[i - 1]
+            else:
+                break
+            i -= 1
+        arr[i] = temp
+
+def merge(arr, left, right, end):
+    merged = []
+    l, r = left, right
+    while l < right and r <= end:
+        if arr[l] <= arr[r]:
+            merged.append(arr[l])
+            l += 1
+        else:
+            merged.append(arr[r])
+            r += 1
+
+    while l < right:
+        merged.append(arr[l])
+        l += 1
+
+    while r <= end:
+        merged.append(arr[r])
+        r += 1
+
+    arr[left : end + 1] = merged
+
+def sort_merge(arr, start, end_inclusive):
+    if end_inclusive <= start: return
+    if end_inclusive == start + 5:
+        sort_insert(arr, start, end_inclusive)
+        return
+
+    middle = (start + end_inclusive) // 2
+    sort_merge(arr, start, middle)
+    sort_merge(arr, middle + 1, end_inclusive)
+    merge(arr, start, middle + 1, end_inclusive)
+
+def partition(arr, left, right):
+    middle = (left + right) // 2
+    pivot = middle
+    if arr[left] > arr[middle]:
+        if arr[middle] > arr[right] : pivot = middle
+        elif arr[left] > arr[right] : pivot = right
+        else : pivot = left
+    else:
+        if(arr[left] > arr[right]) : pivot = left
+        elif(arr[middle] > arr[right]) : pivot = right
+        else : pivot = middle
+    pivot_data = arr[pivot]
+
+    p, q = left, right + 1
+
+    while True:
+        while True:
+            p += 1
+            if p > pivot or arr[p] > pivot_data: break
+
+def sort_quick(arr, start, end_inclusive):
+    if end_inclusive <= start: return
+    if end_inclusive == start + 5:
+        sort_insert(arr, start, end_inclusive)
+        return
+
+    pivot = partition(arr, start, end_inclusive)
+    sort_quick(arr, start, pivot - 1)
+    sort_quick(arr, pivot + 1, end_inclusive)
 
 def main():
     last = len(names) - 1
     sort_insert(names[:], 0, last)
+
+    arr = names[:]
+    print('=' * 60)
+    print(f'ME< {arr}')
+    sort_merge(arr, 0, last)
+    print(f'ME> {arr}')
+
+    arr = names[:]
+    print('=' * 60)
+    print(f'QU< {arr}')
+    sort_quick(arr, 0, last)
+    print(f'QU> {arr}')
+
+    print(f'My name index = {arr.index(names[0])}')
 
 if __name__ == '__main__':
   main()
